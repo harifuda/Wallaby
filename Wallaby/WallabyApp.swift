20d68e5a77247dcb168b68dc41abfede6468678b
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-11-02 08:25:08 -0400

    Initial Commit

---
+++ Wallaby/WallabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallabyApp.swift
+//  Wallaby
+//
+//  Created by Lukas Nguyen on 2022-11-02.
+//
+
+import SwiftUI
+
+@main
+struct WallabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
